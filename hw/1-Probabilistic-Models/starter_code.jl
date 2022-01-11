e6d1b0ca8cac446ba94d1b943d8d4b8f24ec49a0
Zachary Sunberg <zachary.sunberg@colorado.edu>
2022-01-11 10:51:14 -0700

    added hw1 starter code

---
+++ hw/1-Probabilistic-Models/starter_code.jl
@@ -0,0 +1,12 @@
+import DMUStudent.HW1
+
+# Here is a functional but incorrect answer for the programming question
+function f(x)
+    if x > 0
+        return 0
+    else
+        return x
+    end
+end
+
+HW1.evaluate(f, "your.gradescope.email@colorado.edu")
