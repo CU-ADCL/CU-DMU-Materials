55a748c3a7bf3c1431b7da1bdab7cf1a4b64a57e
Zachary Sunberg <zachary.sunberg@colorado.edu>
2023-01-16 18:10:50 -0700

    worked on hw1

--- hw/1-Probabilistic-Models/starter_code.jl
+++ hw/1-Probabilistic-Models/starter_code.jl
@@ -1,12 +1,16 @@
 import DMUStudent.HW1
 
 # Here is a functional but incorrect answer for the programming question
-function f(x)
-    if x > 0
-        return 0
-    else
-        return x
-    end
+function f(a, bs)
+    return a*bs[1]
 end
 
+# You can can test it yourself with inputs like this
+a = [1.0 2.0; 3.0 4.0]
+@show a
+bs = [[1.0, 2.0], [3.0, 4.0]]
+@show bs
+@show f(a, bs)
+
+# This is how you create the json file to submit
 HW1.evaluate(f, "your.gradescope.email@colorado.edu")
