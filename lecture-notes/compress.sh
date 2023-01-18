3413f845bc1b7f943a9c61c5c609cf91b7727071
Zachary Sunberg <zachary.sunberg@colorado.edu>
2023-01-17 17:43:17 -0700

    added intro slides

--- lecture-notes/compress.sh
+++ lecture-notes/compress.sh
@@ -1,5 +1,11 @@
-for file in ./*.pdf
-do
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dBATCH -sOutputFile=tmp.pdf "$file"
-    mv tmp.pdf "$file"
-done
+#!/usr/bin/env bash
+
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dBATCH -sOutputFile=tmp.pdf "$1"
+trash-put "$1"
+mv tmp.pdf "$1"
+
+# for file in ./*.pdf
+# do
+#     gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dBATCH -sOutputFile=tmp.pdf "$file"
+#     mv tmp.pdf "$file"
+# done
