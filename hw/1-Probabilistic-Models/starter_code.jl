83bd8478541085533fa7b7937bae26c634a55578
kranzowe <kranzowe@gmail.com>
2026-01-17 15:39:21 -0700

    291 ns

--- hw/1-Probabilistic-Models/starter_code.jl
+++ hw/1-Probabilistic-Models/starter_code.jl
@@ -1,20 +1,34 @@
 import DMUStudent.HW1
-
+using BenchmarkTools
 #------------- 
 # Problem 4
+# mulitply matrix a (2x2) by a list of vectors (2,)
+#
 #-------------
 
 # Here is a functional but incorrect answer for the programming question
-function f(a, bs)
-    return a*bs[1]
+@inline function f(a::Matrix{T}, bs::Vector{Vector{T}})::Vector{T} where T<:Real
+    # println("Dim of a as run by evaluate is: $(size(a, 1))")
+    # println("Dim of b as run by evaluate is: $(size(bs, 1))")
+
+    if size(bs, 1) == 2
+        # @show hcat(a*bs[1], a*bs[2])
+        return vec(maximum(hcat(a*bs[1], a*bs[2]), dims=2))
+    else
+        return maximum(a*bs[1], dims=2)
+    end
 end
 
 # You can can test it yourself with inputs like this
-a = [1.0 2.0; 3.0 4.0]
-@show a
-bs = [[1.0, 2.0], [3.0, 4.0]]
-@show bs
-@show f(a, bs)
+# a = [2.0 0.0; 0.0 1.0]
+# @show a
+# bs = [[1.0, 2.0], [2.0, 1.0]]
+# @show bs
+# @show f(a, bs)
+
+a = rand(2,2)
+bs = [rand(2) for _ in 1:100]
+@btime f($a, $bs)
 
 # This is how you create the json file to submit
-HW1.evaluate(f, "your.gradescope.email@colorado.edu")
+#HW1.evaluate(f, "owen.kranz@colorado.edu")
